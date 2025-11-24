341f926562482aa54cacf212fd03c9baa02a807a
Pratyush <157958382+prates09@users.noreply.github.com>
2025-11-24 13:55:59 +0530

    Project upload: .NET 8 minimal API + InMemory DB + xUnit tests

---
+++ PolicyNotesService/PolicyNotesService/Repositories/PolicyNoteRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PolicyNotesService.Data;
+using PolicyNotesService.Models;
+
+namespace PolicyNotesService.Repositories
+{
+    public class PolicyNoteRepository : IPolicyNoteRepository
+    {
+        private readonly PolicyNotesDbContext _db;
+
+        public PolicyNoteRepository(PolicyNotesDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<PolicyNote> AddAsync(PolicyNote note)
+        {
+            _db.PolicyNotes.Add(note);
+            await _db.SaveChangesAsync();
+            return note;
+        }
+
+        public Task<List<PolicyNote>> GetAllAsync()
+        {
+            return _db.PolicyNotes
+                      .AsNoTracking()
+                      .OrderBy(n => n.Id)
+                      .ToListAsync();
+        }
+
+        public Task<PolicyNote?> GetByIdAsync(int id)
+        {
+            return _db.PolicyNotes
+                      .AsNoTracking()
+                      .FirstOrDefaultAsync(n => n.Id == id);
+        }
+    }
+}
